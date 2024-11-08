a9ca4d0bd1c05231257fc6f3eefe2734978f3e45
kalpitaChaudhuri <kalpitachaudhuri01@gmail.com>
2024-11-08 20:21:50 +0530

    first commit

---
+++ src/main/java/com/example/demo/Bankingapp1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bankingapp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bankingapp1Application.class, args);
+	}
+
+}
