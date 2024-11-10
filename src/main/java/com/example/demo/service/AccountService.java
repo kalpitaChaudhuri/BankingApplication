16ad4ddd848c203c6ff98d54532953f4a75db61d
kalpitaChaudhuri <kalpitachaudhuri01@gmail.com>
2024-11-10 20:22:51 +0530

    create account

---
+++ src/main/java/com/example/demo/service/AccountService.java
@@ -0,0 +1,12 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.accdto.AccountDto;
+
+@Service
+public interface AccountService {
+
+	AccountDto createAccount(AccountDto accountdto);
+	
+}
---
+++ src/main/java/com/example/demo/service/implementation/AccountServiceImplementation.java
@@ -0,0 +1,25 @@
+package com.example.demo.service.implementation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.example.demo.accdto.AccountDto;
+import com.example.demo.entity.Accounts;
+import com.example.demo.mapper.AccountMapper;
+import com.example.demo.repository.AccountRepository;
+import com.example.demo.service.AccountService;
+
+@Component
+public class AccountServiceImplementation implements AccountService{
+
+	@Autowired
+	AccountRepository accountrepository;
+	
+	@Override
+	public AccountDto createAccount(AccountDto accountdto) {
+		Accounts accounts=AccountMapper.maptoaccount(accountdto);
+		Accounts accountsaved=accountrepository.save(accounts);
+		return AccountMapper.maptoaccountdto(accountsaved);
+	}
+
+}
