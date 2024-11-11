e4f27e2c19f618c36e5cfc7999314490591487c2
kalpitaChaudhuri <kalpitachaudhuri01@gmail.com>
2024-11-11 18:53:20 +0530

    get controller

--- src/main/java/com/example/demo/controller/AccountController.java
+++ src/main/java/com/example/demo/controller/AccountController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,4 +25,9 @@ public class AccountController {
 		return new ResponseEntity<AccountDto>(accountservice.createAccount(accountdto), HttpStatus.CREATED);
 	}
 	
+	@GetMapping("/get/{id}")
+	public ResponseEntity<AccountDto> getAccountById(@PathVariable int id) {
+		AccountDto accountdto=accountservice.getAccountById(id);
+		return ResponseEntity.ok(accountdto);
+	}
 }
