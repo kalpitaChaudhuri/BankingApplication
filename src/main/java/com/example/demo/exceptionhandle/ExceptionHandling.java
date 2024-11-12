a7b246f6826f235fa8459f11532208beca89b752
kalpitaChaudhuri <kalpitachaudhuri01@gmail.com>
2024-11-12 14:55:09 +0530

    minor

--- src/main/java/com/example/demo/exceptionhandle/ExceptionHandling.java
+++ src/main/java/com/example/demo/exceptionhandle/ExceptionHandling.java
@@ -4,10 +4,10 @@ import javax.security.auth.login.AccountNotFoundException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ExceptionHandling {
 
 	@ExceptionHandler(AccountNotFoundException.class)
