d5695e296efa63c0230b96a385a89ee5c017fb1a
kalpitaChaudhuri <kalpitachaudhuri01@gmail.com>
2024-11-12 19:22:57 +0530

    exception handled

--- src/main/java/com/example/demo/exceptionhandle/ExceptionHandling.java
+++ src/main/java/com/example/demo/exceptionhandle/ExceptionHandling.java
@@ -9,6 +9,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ExceptionHandling {
+	
+	@ExceptionHandler(RuntimeException.class)
+	public ResponseEntity<Response> handleRuntimeException(RuntimeException e) {
+		Response response=new Response(e.getMessage(), HttpStatus.NOT_FOUND.value());
+		return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+	}
 
 	@ExceptionHandler(AccountNotFoundException.class)
 	public ResponseEntity<Response> handleAccountNotFound(AccountNotFoundException e) {
