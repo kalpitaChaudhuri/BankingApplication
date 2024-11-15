2aeea8279c3119bf38c52bd737365452ce02261a
kalpitaChaudhuri <kalpitachaudhuri01@gmail.com>
2024-11-15 22:05:17 +0530

    withdraw added

--- src/main/java/com/example/demo/service/AccountService.java
+++ src/main/java/com/example/demo/service/AccountService.java
@@ -18,4 +18,6 @@ public interface AccountService {
 	List<AccountDto> getAccountByAge(int age) throws Exception;
 	
 	AccountDto deposit(int id, double amount);
+	
+	AccountDto withdraw(int id ,double amount);
 }
