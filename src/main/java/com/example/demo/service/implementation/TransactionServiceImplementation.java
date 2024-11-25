6bbef2c80af647bc943b0e91465f3a58b64384c4
kalpitaChaudhuri <kalpitachaudhuri01@gmail.com>
2024-11-25 12:28:09 +0530

    transaction save method imple

--- src/main/java/com/example/demo/service/implementation/TransactionServiceImplementation.java
+++ src/main/java/com/example/demo/service/implementation/TransactionServiceImplementation.java
@@ -3,11 +3,20 @@ package com.example.demo.service.implementation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.example.demo.entity.Transactions;
 import com.example.demo.repository.TransactionRepository;
+import com.example.demo.service.TransactionService;
 
 @Component
-public class TransactionServiceImplementation implements TransactionRepository{
+public class TransactionServiceImplementation implements TransactionService{
 
 	@Autowired
 	private TransactionRepository transactionrepository;
+
+	@Override
+	public Transactions saveTransaction(Transactions transaction) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
