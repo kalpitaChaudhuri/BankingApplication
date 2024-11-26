37441024a0982f0482c43ca181bf3bce98985e73
kalpitaChaudhuri <kalpitachaudhuri01@gmail.com>
2024-11-26 16:28:10 +0530

    changes in accounts

--- src/main/java/com/example/demo/entity/Accounts.java
+++ src/main/java/com/example/demo/entity/Accounts.java
@@ -1,9 +1,14 @@
 package com.example.demo.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Accounts {
@@ -14,40 +19,62 @@ public class Accounts {
 	private String name;
 	private int age;
 	private double balance;
+	
+	@OneToMany(mappedBy = "account", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Transactions> transactions = new ArrayList<>();
+
 	public Accounts() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public Accounts(int id, String name, int age, double balance) {
+
+	public Accounts(int id, String name, int age, double balance, List<Transactions> transactions) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.age = age;
 		this.balance = balance;
+		this.transactions = transactions;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getAge() {
 		return age;
 	}
+
 	public void setAge(int age) {
 		this.age = age;
 	}
+
 	public double getBalance() {
 		return balance;
 	}
+
 	public void setBalance(double balance) {
 		this.balance = balance;
+	}
+
+	public List<Transactions> getTransactions() {
+		return transactions;
+	}
+
+	public void setTransactions(List<Transactions> transactions) {
+		this.transactions = transactions;
 	}
 	
 }
