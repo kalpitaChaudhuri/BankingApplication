745f242e4df9f6a081ace08700d9dcdb8a413535
kalpitaChaudhuri <kalpitachaudhuri01@gmail.com>
2024-11-26 16:03:47 +0530

    save transaction controller

--- src/main/java/com/example/demo/controller/TransactionController.java
+++ src/main/java/com/example/demo/controller/TransactionController.java
@@ -1,9 +1,13 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.entity.Transactions;
 import com.example.demo.service.TransactionService;
 
 @RestController
@@ -13,5 +17,9 @@ public class TransactionController {
 	@Autowired
 	private TransactionService transactionService;
 
-	
+	 @PostMapping
+	    public ResponseEntity<Transactions> saveTransaction(@RequestBody Transactions transaction) {
+		 Transactions savedTransaction = transactionService.saveTransaction(transaction);
+	        return ResponseEntity.ok(savedTransaction);
+	    }
 }
